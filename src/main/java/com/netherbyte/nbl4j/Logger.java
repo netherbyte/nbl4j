48dbfcbdd018e559c660aff43c0bf857464a5cf1
Sid <saytleader@outlook.com>
2023-06-08 10:25:42 -0400

    Bump version number

--- src/main/java/com/netherbyte/nbl4j/Logger.java
+++ src/main/java/com/netherbyte/nbl4j/Logger.java
@@ -1,10 +1,44 @@
 package com.netherbyte.nbl4j;
 
 public interface Logger {
+    /**
+     * @return Name of the logger instance
+     * @since 1.0
+     */
     public String getLoggerName();
-    public void debug(String message);
-    public void info(String message);
-    public void warn(String message);
-    public void error(String message);
-    public void critical(String message);
+
+    /**
+     * Prints a message to the DEBUG channel
+     * @param x The message to log
+     * @since 1.0
+     */
+    public void debug(String x);
+
+    /**
+     * Prints a message to the INFO channel
+     * @param x The message to log
+     * @since 1.0
+     */
+    public void info(String x);
+
+    /**
+     * Prints a message to the WARN channel
+     * @param x The message to log
+     * @since 1.0
+     */
+    public void warn(String x);
+
+    /**
+     * Prints a message to the ERROR channel
+     * @param x The message to log
+     * @since 1.0
+     */
+    public void error(String x);
+
+    /**
+     * Prints a message to the CRITICAL channel and stops the program
+     * @param x The message to log
+     * @since 1.0
+     */
+    public void critical(String x);
 }
